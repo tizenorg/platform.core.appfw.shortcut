457172da3a84438530461b35a7a323979ecbb21f
Sung-jae Park <nicesj.park@samsung.com>
2011-11-10 14:08:52 +0900

    Update PG

---
+++ include/SLP_shortcut_PG.h
@@ -0,0 +1,64 @@
+/*
+ * shortcut
+ *
+ * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Sung-jae Park <nicesj.park@samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/**
+ *  @ingroup SLP_PG
+ *  @defgroup SHORTCUT_PG Add to home (shortcut)
+ *  @{
+
+ <h1 class="pg">Introduction</h1>
+ Shortcut is a communication interfaces for homescreen and applications. To enable the add_to_home feature. Shortcut(libslp-shortcut) uses socket for IPC.
+
+ @image html SLP_Shortcut_PG_image01.png
+
+ <h2 class="pg">Properties</h2>
+- Types of shortcut
+- Header File : shortcut.h 
+
+<h1 class="pg">Programming Guide</h1>
+
+<h2 class="pg">Types of shortcut</h1>
+Shortcut defines 3 kinds of types.
+
+<h3 class="pg">1.	SHORTCUT_PACKAGE</h1>
+We can use this for adding a package shortcut with additional information. <br/>
+@code
+@endcode
+
+<h3 class="pg">2.	SHORTCUT_DATA</h1>
+@code
+@endcode
+
+<h3 class="pg">3.	SHORTCUT_FILE</h1>
+@code
+@endcode
+
+<h2 class="pg">Notification</h1>
+@code
+@endcode
+*/
+
+/**
+@}
+*/
+
