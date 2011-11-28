e46ec352ba4c527bf7333d2b91970dc7b5dbf6ad
Sung-jae Park <nicesj.park@samsung.com>
2011-11-28 16:55:15 +0900

    Update comment for doxygen.
    Change-Id: Ibfd9789ad88386f76e18da57b7576c13e9b8837d

--- include/shortcut.h
+++ include/shortcut.h
@@ -1,9 +1,10 @@
 /*
- * shortcut
+ * [shortcut]
  *
  * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
  *
- * Contact: Sung-jae Park <nicesj.park@samsung.com>
+ * Contact: Sung-jae Park <nicesj.park@samsung.com>,
+ *          Youngjoo Park <yjoo93.park@samsung.com>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the
@@ -33,15 +34,15 @@
  * @defgroup SHORTCUT Add to home (shortcut)
  * @author Sung-jae Park <nicesj.park@samsung.com>
  * @version 0.1
- * @brief To support the add_to_home feature. Two types of APIs are supported.
+ * @brief To enhance the Add to home feature. Two types of API set are supported.
  *        One for the homescreen developers.
- *        The others for the application developers who should to implement the add_to_home feature.
+ *        The others for the application developers who should implement the Add to home feature.
  */
 
 
 /**
  * @brief This function prototype is used to define a callback function for the add_to_home reqeust.
- *        The homescreen should to define a callback as this type and implementing the service code
+ *        The homescreen should define a callback as this type and implementing the service code
  *        for adding a new application shortcut.
  * @param[in] pkgname Shortcut is added for this package.
  * @param[in] name Name for created shortcut icon.
@@ -50,7 +51,7 @@
  * @param[in] icon Absolute path of an icon file for this shortcut.
  * @param[in] pid Process ID of who request add_to_home.
  * @param[in] data Callback data.
- * @return int Developer should to returns the result of handling shortcut creation request.
+ * @return int Developer should returns the result of handling shortcut creation request.
  *             Returns 0, if succeed to handles the add_to_home request, or returns proper errno.
  * @see shortcut_set_request_cb
  * @pre None
@@ -74,7 +75,7 @@ typedef int (*result_cb_t)(int ret, int pid, void *data);
 
 /**
  * @brief Basically, three types of shortcut is defined.
- *        Every homescreen developer should to support these types of shortcut.
+ *        Every homescreen developer should support these types of shortcut.
  *        Or returns proper errno to figure out why the application failed to add a shortcut.
  *        SHORTCUT_PACKAGE is used for adding a package itself as a shortcut
  *        SHORTCUT_DATA is used for adding a shortcut for "content" data.
@@ -87,16 +88,38 @@ enum {
 };
 
 /**
- * @brief Homescreen should to use this function to service the shortcut creating request.
+ * @fn int shortcut_set_request_cb(request_cb_t request_cb, void *data)
+ *
+ * @brief Homescreen should use this function to service the shortcut creating request.
+ *
+ * @par Sync (or) Async:
+ * This is an asynchronous API.
+ *
+ * @par Important Notes:
+ * - Should be used from the homescreen.
+ * - Should check the return value of this function
+ *
  * @param[in] request_cb Callback function pointer which will be invoked when add_to_home is requested.
  * @param[in] data Callback data to deliver to the callback function.
- * @return void There is no return value.
+ *
+ * @return Return Type (int)
+ * - 0 - callback function is successfully registered
+ * - < 0 - Failed to register the callback function for request.
+ *
  * @see request_cb_t
- * @pre None
- * @post None
- * @remarks None
+ *
+ * @pre - You have to prepare a callback function
+ *
+ * @post - If a request is sent from the application, the registered callback will be invoked.
+ *
+ * @remarks - None
+ *
+ * @par Prospective Clients:
+ * Homescreen
+ *
  * @par Example
  * @code
+ * #include <shortcut.h>
  *
  * static int request_cb(const char *pkgname, const char *name, int type, const char *content_info, const char *icon, int pid, void *data)
  * {
@@ -126,7 +149,18 @@ enum {
 extern int shortcut_set_request_cb(request_cb_t request_cb, void *data);
 
 /**
- * @brief The application, which supporting the add_to_home feature, should to invoke this.
+ * @fn int shortcut_add_to_home(const char *pkgname, const char *name, int type, const char *content_info, const char *icon, result_cb_t result_cb, void *data)
+ *
+ * @brief The application, which supporting the add_to_home feature, should invoke this.
+ *
+ * @par Sync (or) Async:
+ * This is an asynchronous API.
+ *
+ * @par Important Notes:
+ * - Application should check the return value of this function.
+ * - Application should check the return status from the callback function
+ * - Application should set the callback function to get the result of this request.
+ *
  * @param[in] pkgname Package name of owner of this shortcut.
  * @param[in] name Name for created shortcut icon.
  * @param[in] type 3 kinds of types are defined.
@@ -134,15 +168,34 @@ extern int shortcut_set_request_cb(request_cb_t request_cb, void *data);
  * @param[in] icon Absolute path of an icon file
  * @param[in] result_cb Callback function pointer which will be invoked after add_to_home request.
  * @param[in] data Callback data to deliver to the callback function.
- * @return int Returns 0 if it succeed to send the request, or returns proper errno.
+ *
+ * @return Return Type (int)
+ * - 0 - Succeed to send the request
+ * - <0 - Failed to send the request
+ *
  * @see result_cb_t
- * @pre None
- * @post None
- * @remarks None
+ *
+ * @pre - You have to prepare the callback function
+ *
+ * @post - You have to check the return status from callback function which is passed by argument.
+ *
+ * @remarks - If a homescreen does not support this feature, you will get proper error code.
+ *
+ * @par Prospective Clients:
+ * Inhouse Apps.
+ *
  * @par Example
  * @code
+ *
+ * #include <stdio.h>
+ * #include <shortcut.h>
+ *
  * static int result_cb(int ret, int pid, void *data)
  * {
+ * 	if (ret < 0)
+ * 		printf("Failed to add a shortcut: %s\n", perror(ret));
+ *
+ *	printf("Processed by the %d\n", pid);
  * 	return 0;
  * }
  *
