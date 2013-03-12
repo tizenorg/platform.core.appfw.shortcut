0a52e4e6079e7b2342c10e1a818bc5b6d8684b2c
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-13 01:46:38 +0900

    Tizen 2.1 base

---
+++ include/SLP_shortcut_PG.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012  Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.tizenopensource.org/license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *  @ingroup SLP_PG
+ *  @defgroup SHORTCUT_PG Add to home (shortcut)
+ *  @{
+
+ <h1 class="pg">Introduction</h1>
+ Shortcut is a communication interfaces for homescreen and applications. To enable the add_to_home feature. Shortcut(libslp-shortcut) uses socket for IPC.
+
+ @image html SLP_Shortcut_PG_image01.png
+
+ <h2 class="pg">Properties</h2>
+- Types of shortcut
+- Header File : shortcut.h 
+
+<h1 class="pg">Programming Guide</h1>
+
+<h2 class="pg">Types of shortcut</h1>
+Shortcut defines 3 kinds of types.
+
+<h3 class="pg">1.	SHORTCUT_PACKAGE</h1>
+We can use this for adding a package shortcut with additional information. <br/>
+@code
+@endcode
+
+<h3 class="pg">2.	SHORTCUT_DATA</h1>
+@code
+@endcode
+
+<h3 class="pg">3.	SHORTCUT_FILE</h1>
+@code
+@endcode
+
+<h2 class="pg">Error code</h1>
+<h3 class="pg">1.	-EINVAL</h1>
+<h3 class="pg">1.	-EFAULT</h1>
+@code
+@endcode
+*/
+
+/**
+@}
+*/
---
+++ test/application.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012  Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.tizenopensource.org/license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <Elementary.h>
+#include <shortcut.h>
+
+static int result_cb(int ret, int pid, void *data)
+{
+	printf("Client: Return %d (%d)\n", ret, pid);
+	//elm_exit();
+	return 0;
+}
+
+static Eina_Bool shortcut_add_cb(void *data)
+{
+	int ret;
+
+	ret = shortcut_add_to_home("MyName", 0, "/usr/bin/true", "/opt/share/image/what.png", result_cb, NULL);
+	printf("Client: shortcut_add_to_home returns: %d\n", ret);
+
+	return ECORE_CALLBACK_RENEW;
+}
+
+int elm_main(int argc, char *argv[])
+{
+	Ecore_Timer *timer;
+
+	timer = ecore_timer_add(3.0f, shortcut_add_cb, NULL);
+	if (!timer) {
+		printf("Failed to add a timer\n");
+	}
+
+	elm_run();
+	elm_shutdown();
+
+	return 0;
+}
+
+ELM_MAIN()
+/* End of a file */
