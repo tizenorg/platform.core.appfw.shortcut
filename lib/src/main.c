73841e54e490d5b718922726fd1fa2c7f753dacc
Sung-jae Park <nicesj.park@samsung.com>
2013-05-15 11:13:11 +0900

    Icon service API is introduced.
    Patch 2
        Update the icon service reuqest codes.
        APIs are changed.
    
    Patch 1
        Update shortcut service library.
        Icon service API is implemented.
        Default layout & group should be "" (zero length string)
        The icon provider will choose the default layout and group when it get zero length string.
    
    Change-Id: I2d47a1dbdeb8281c20f3a352fd3bcaf4ffe10a4e

--- lib/src/main.c
+++ lib/src/main.c
@@ -35,22 +35,7 @@
 #include <com-core_packet.h>
 
 #include "shortcut.h"
-
-#if !defined(FLOG)
-#define DbgPrint(format, arg...)	LOGD("[[32m%s/%s[0m:%d] " format, basename(__FILE__), __func__, __LINE__, ##arg)
-#define ErrPrint(format, arg...)	LOGE("[[32m%s/%s[0m:%d] " format, basename(__FILE__), __func__, __LINE__, ##arg)
-#else
-extern FILE *__file_log_fp;
-#define DbgPrint(format, arg...) do { fprintf(__file_log_fp, "[LOG] [[32m%s/%s[0m:%d] " format, basename(__FILE__), __func__, __LINE__, ##arg); fflush(__file_log_fp); } while (0)
-
-#define ErrPrint(format, arg...) do { fprintf(__file_log_fp, "[ERR] [[32m%s/%s[0m:%d] " format, basename(__FILE__), __func__, __LINE__, ##arg); fflush(__file_log_fp); } while (0)
-#endif
-
-#define EAPI __attribute__((visibility("default")))
-
-#if !defined(VCONFKEY_MASTER_STARTED)
-#define VCONFKEY_MASTER_STARTED	"memory/data-provider-master/started"
-#endif
+#include "shortcut_internal.h"
 
 int errno;
 
--- packaging/libshortcut.spec
+++ packaging/libshortcut.spec
@@ -1,6 +1,6 @@
 Name: libshortcut
-Summary: Shortcut add/remove request function supporting library
-Version: 0.5.1
+Summary: Shortcut add feature supporting library
+Version: 0.6.1
 Release: 0
 Group: HomeTF/Framework
 License: Apache License
